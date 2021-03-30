618818a8b28145efd50e8e385b0491aa1e5a052b
Ryusuke Onose <gibson.red.gin.suke@gmail.com>
2021-03-30 13:23:25 +0900

    【ADD】material-ui

--- front/react-sample/src/App.js
+++ front/react-sample/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-function App() {
+const useStyles = makeStyles({
+  button: {
+    backgroundColor: 'red'
+  }
+});
+
+const MaterialUIHooks = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button variant="contained" className={classes.button}>
+      Test
+    </Button>
   );
 }
 
-export default App;
+export default MaterialUIHooks;
